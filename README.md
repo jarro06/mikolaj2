c1947fd21136c69a2af81313082a49f8fd559086
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-12-08 10:00:54 +0100

    Create maven project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# mikolaj2
-This is Mój super nowy projekt
+# maven_template
+This is testow2o12345
---
+++ src/main/java/de/allianz/mikolaj2/Mikolaj2Application.java
@@ -0,0 +1,13 @@
+package de.allianz.mikolaj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mikolaj2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mikolaj2Application.class, args);
+	}
+
+}
