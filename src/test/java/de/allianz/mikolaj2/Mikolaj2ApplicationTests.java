5b0df1bb0fd0df54b73c51b296768021a9d0a89b
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-12-08 10:03:27 +0100

    Create maven project

---
+++ src/test/java/de/allianz/mikolaj2/Mikolaj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.mikolaj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mikolaj2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
